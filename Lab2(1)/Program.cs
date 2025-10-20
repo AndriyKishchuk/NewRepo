9b0e62bff0d58aa3b59a0f34cf076c1fe7f6cbc2
w72228 <andriy302007@gmail.com>
2025-10-20 13:47:26 +0200

    add

---
+++ Lab2(1)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baker baker = new Baker();
+            baker.Work();
+            // Employee employee = new Employee() // Error - Because Employee is abstract and objects of abstract classes cannot be created.
+            Console.ReadKey();
+        }
+    }
+    abstract class Employee
+    {
+        public abstract void Work();
+    }
+    class Baker : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Baking bread");
+        }
+    }
+
+}
--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,2 +1,77 @@
-﻿
-Console.WriteLine("Hello world");
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Animal animal = new Animal("Burek");
+            Dog dog = new Dog("Rex");
+            Kot kot = new Kot("Mruczek");
+            Wez wez = new Wez("Kaa");
+
+            static void powiedz_cos(Animal animal)
+            {
+                animal.daj_glos();
+                Console.WriteLine($"Typ obiektu: {animal.GetType().Name}");
+            }
+
+           // powiedz_cos(animal);
+            powiedz_cos(dog);
+            powiedz_cos(kot);
+            powiedz_cos(wez);
+            
+
+        }
+    }
+    public class Animal
+    {
+        protected string name;
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void daj_glos()
+        {
+            Console.WriteLine(".....");
+        }
+    }
+    public class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+            this.name = name;
+        }
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{name}-Hau Hau");
+        }
+    }
+    public class Kot : Animal
+    {
+        public Kot(string name) : base(name)
+        {
+            this.name = name;
+        }
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{name}-Miu miu");
+        }
+    }
+    public class Wez : Animal
+    {
+        public Wez(string name) : base(name)
+        {
+            this.name = name;
+        }
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{name}-Ssssss");
+
+        }
+    }
+}
+
