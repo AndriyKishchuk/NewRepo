1a8a35ebf4f9bcad257ed99e7d109a863d7b6adc
w72228 <andriy302007@gmail.com>
2025-10-20 14:19:14 +0200

    add

---
+++ Lab2(2)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            Console.ReadKey();
+        }
+    }
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("To jest konstruktor A");
+        }
+
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("To jest konstruktor B");
+        }
+    }
+    class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("To jest konsstruktor C ");
+        }
+    }
+}
